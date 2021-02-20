9d8b0f981e586cdf8204fcea5a8dc937198a8b4f
asanch31 <78468358+asanch31@users.noreply.github.com>
2021-02-19 20:32:24 -0800

    Created basic map w/ player movement

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //intialize float to allow user control of car(object) speed.
+    public float speed = 15f;
+    //intialize float to allow user control of car(object) turnspeed
+    public float turnSpeed = 20f;
+
+    //allow player to control object with userinput
+    private float horizontalInput;
+    private float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            transform.Translate(Vector3.up * Time.deltaTime * speed * verticalInput);
+        if (Input.GetKey(KeyCode.R))
+            transform.Rotate(0, 0, 0);
+
+        //allow player to control object with userinput
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        // move car based on speed variable and user input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        // turn car based on speed variable and user input
+        //transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+        float mxVal = Input.GetAxis("Mouse X");
+        float myVal = Input.GetAxis("Mouse Y");
+        if (mxVal > 0)
+            transform.Rotate(0f, 1f, 0f);
+        if (mxVal < 0)
+            transform.Rotate(0f, -1f, 0f);
+       
+        
+    }
+}
---
+++ Assets/scripts/playerCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCamera : MonoBehaviour
+{
+    //allow object to connect with player object
+    public GameObject player;
+    // offset camera from player position (third person view)
+    private Vector3 offset = new Vector3(0, 4, -12);
+    // Start is called before the first frame update
+    private float horizontalInput;
+    void Start()
+    {
+
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //allow camera to follow object with offset for third person view
+        transform.position = player.transform.position;
+        transform.Rotate(Vector3.up * Time.deltaTime * 20 * horizontalInput);
+    }
+}
